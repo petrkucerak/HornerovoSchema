f620e5ea4129f49956dad89d8230a187c5e16028
Petr Kučera <45851215+petrkucerak@users.noreply.github.com>
2020-10-15 21:49:23 +0200

    Dokonceni Hornerova schematu. Otestovano na zakladnim polynomu, bez debugu a podminek

--- main.py
+++ main.py
@@ -52,6 +52,65 @@ class Polynom:
                 meta_number.append(divisor[i])
         return meta_number
 
+    """
+    Hlavni algrotimus tz.n Hronerovo schema v praxi
+    """
+
+    # vypise nejmensi tvar polynomu
+    def short_polynom(self):
+        meta_number = self.find_meta_number()
+        new_polynom = []
+
+        # pouyivame k definovani prvniho clenu Hornerova schematu
+        is_first = True
+
+        # cyklus se zopakuje podle poctu korenu
+        for i in range(0, len(meta_number)):
+
+            # cyklus projizdi jednotlive koeficienty a dopocitava nove pole
+            for s in range(0, len(self.polynom)):
+                
+                if is_first == True:
+                    new_polynom.append(self.polynom[s])
+                    is_first = False
+                else:    
+                    new_polynom.append( (meta_number[i] * new_polynom[s-1]) + self.polynom[s] )
+            
+            is_first = True
+
+            self.polynom = new_polynom
+            new_polynom = []
+
+        # cyklus zajistuje vraceni pouze cesti s hodnotami
+        part_polynom = []
+        for i in range(0, (len(self.polynom)-len(meta_number))):
+            part_polynom.append(self.polynom[i])
+
+        # ==========================
+        # vypise finalni zkraceny format
+
+        out = ""
+
+        # cast s koreny
+        for i in range(0, len(meta_number)):
+            if meta_number[i] < 0:
+                out += "(x" + str(meta_number[i]) + ")"
+
+            else:
+                out += "(x+" + str(meta_number[i]) + ")"
+        
+        # cast nerozkladatelna
+        out += "("
+        for i in range(0, len(part_polynom)):
+            if part_polynom[i] != 0:
+                if part_polynom[i] > 0:
+                    out += "+"    
+                out += str(part_polynom[i]) + "x^"
+                out += str(len(part_polynom)-i-1)
+        out += ")"
+
+        return out
+
 
 if __name__ == '__main__':
     c1 = Polynom([1, -4, 2, 2, 1, 6])
@@ -59,6 +118,9 @@ if __name__ == '__main__':
 
     c2 = c1.find_meta_number()
     print(c2)
+
+    c3 = c1.short_polynom()
+    print(c3)
 
 # input user polynom
 # input specific number type R, C, N, Q, ... (default is R)
